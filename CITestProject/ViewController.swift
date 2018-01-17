56b859b30123e3d020df79c14918de8aacf9cab2
John Brunelle <johnbrunelle@Johns-MacBook-Pro.local>
2018-01-17 12:43:32 -0500

    readded project

---
+++ CITestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CITestProject
+//
+//  Created by John Brunelle on 1/17/18.
+//  Copyright © 2018 Adept Mobile, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
